597d353a8771d31691c962ccb4cd740a1a57bbc4
Muhammad Ibtsaam Qadir <66183292+ibtsaamqadir@users.noreply.github.com>
2024-08-08 20:21:07 -0400

    Update polynomial_regression.py

--- Polynomial-Regression/polynomial_regression.py
+++ Polynomial-Regression/polynomial_regression.py
@@ -30,52 +30,32 @@ def gradientDescentVectorize(X, y, theta, alpha, num_iters):
         J_history.append(computeCostVectorize(X, y, theta))
     
     return theta, J_history
-
-X, Y = data[:, 0], data[:, 1]    
 m=len(data)
 X=np.stack([np.ones(m),X, np.power(X,2)],axis=1)
 
 cost= computeCostVectorize(X, Y, theta=np.array([0.0, 0.0,0.0]))
-print("The cost is",cost )
-
 theta_2 = np.zeros(3)
-
-# some gradient descent settings
 iterations = 1500
 alpha = 0.00001
 
 theta_2, J_history = gradientDescentVectorize(X ,Y, theta_2, alpha, iterations) 
-print(theta_2)
-# plot the 2nd Order fit
-plt.scatter(X[:, 1],Y, marker = 'x', color = 'green')
+
 p = np.linspace(5, 25, 1000)
 p=np.stack([np.ones(1000),p, np.power(p,2)],axis=1)
 
 plt.plot(p[:, 1], np.dot(p, theta_2))  
 plt.legend(['Training data', 'Second order Polynomial']);
 
-X, Y = data[:, 0], data[:, 1]
-m=len(data)
-
 X=np.stack([np.ones(m),X, np.power(X,2),np.power(X,3)],axis=1)
-
 cost= computeCostVectorize(X, Y, theta=np.array([0.0, 0.0,0.0,0.0]))
-print("The cost is",cost )
-
 theta_3 = np.zeros(4)
-
-# some gradient descent settings
 iterations = 1500
 alpha = 0.00000001
 
 theta_3, J_history = gradientDescentVectorize(X ,Y, theta_3, alpha, iterations) 
-print(theta_3)
-# plot the 3rd order polynomial fit
-plt.scatter(X[:, 1],Y, marker = 'x', color = 'orange')
 
+plt.scatter(X[:, 1],Y, marker = 'x', color = 'orange')
 p = np.linspace(5, 25, 1000)
 p=np.stack([np.ones(1000),p, np.power(p,2),np.power(p,3)],axis=1)
-
 plt.plot(p[:, 1], np.dot(p, theta_3)) 
-
 plt.legend(['Training data', 'Third order Polynomial'])
